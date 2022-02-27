9029fcd59ef045a0e4d5add9695e31fb98876b44
Lanre039 <rajilat1000@gmail.com>
2022-02-27 23:39:34 +0100

    added search by tags

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -3,6 +3,7 @@ import { useState } from "react";
 const useSearch = () => {
   const [listOfAuthors, setListOfAuthors] = useState([]);
   const [listOfGenres, setListOfGenres] = useState([]);
+  const [listOfTags, setListOfTags] = useState([]);
 
   const getListOfAuthors = (books) => {
     if (listOfAuthors.length) {
@@ -18,6 +19,19 @@ const useSearch = () => {
     return store;
   };
 
+  const getListOfTags = (books) => {
+    if (listOfTags.length) {
+      return listOfTags;
+    }
+    let store = [];
+    books.forEach(({ tags }, idx) => {
+      for (let tag of tags) {
+        store = [...store, { ...tag, bookIndex: idx }];
+      }
+    });
+    setListOfTags(store);
+    return store;
+  };
   const getListOfGenres = (books) => {
     if (listOfGenres.length) {
       return listOfGenres;
@@ -42,28 +56,45 @@ const useSearch = () => {
 
     if (searchByTitle.length) {
       return searchByTitle;
-    } else {
-      // SEARCH BY AUTHOR
-      const authors = getListOfAuthors(books);
-      const searchByAuthor = authors.filter(({ name }) => {
-        return search
-          ? RegExp(`^${search}`, "ig").test(name.trim().toLowerCase())
-          : true;
-      });
-      if (searchByAuthor.length) {
-        const filter = searchByAuthor.map(({ bookIndex }) => books[bookIndex]);
-        return filter;
-      } else {
-        //SEARCH BY GENRES
-        const genres = getListOfGenres(books);
-        const searchByGenres = genres.filter(({ name }) => {
-          return search
-            ? RegExp(`^${search}`, "ig").test(name.trim().toLowerCase())
-            : true;
-        });
-        const filter = searchByGenres.map(({ bookIndex }) => books[bookIndex]);
-        return filter;
-      }
+    }
+
+    // SEARCH BY AUTHOR
+    const authors = getListOfAuthors(books);
+    const searchByAuthor = authors.filter(({ name }) => {
+      return search
+        ? RegExp(`^${search}`, "ig").test(name.trim().toLowerCase())
+        : true;
+    });
+
+    if (searchByAuthor.length) {
+      const filter = searchByAuthor.map(({ bookIndex }) => books[bookIndex]);
+      return filter;
+    }
+
+    // SEARCH BY TAGS
+    const tags = getListOfTags(books);
+    const searchByTags = tags.filter(({ name }) => {
+      return search
+        ? RegExp(`^${search}`, "ig").test(name.trim().toLowerCase())
+        : true;
+    });
+
+    if (searchByTags.length) {
+      const filter = searchByTags.map(({ bookIndex }) => books[bookIndex]);
+      return filter;
+    }
+
+    //SEARCH BY GENRES
+    const genres = getListOfGenres(books);
+    const searchByGenres = genres.filter(({ name }) => {
+      return search
+        ? RegExp(`^${search}`, "ig").test(name.trim().toLowerCase())
+        : true;
+    });
+
+    if (searchByGenres.length) {
+      const filter = searchByGenres.map(({ bookIndex }) => books[bookIndex]);
+      return filter;
     }
   };
 
